d0b02952695f83c9ddb8ea615fc0aa719eea2965
Rafael Barros <rafaelbarros.df@gmail.com>
2018-12-22 14:43:26 -0200

    Criando nosso proprio Array.Reduce

---
+++ modulo-03/04-lists/for-forin/04.reduce.js
@@ -0,0 +1,39 @@
+const { obterPessoas } = require('./service')
+
+Array.prototype.meuReduce = function (callback, valorInicial) {
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0]
+    for (let index = 0; index <= this.length - 1; index++) {
+        valorFinal = callback(valorFinal, this[index], this)
+    }
+    return valorFinal;
+}
+
+async function main() {
+    try {
+        const { results } = await obterPessoas('a');
+        // const pesos = results.map(item => parseInt(item.height))
+        // [ 20.2, 30.3, 40.5] = 0
+
+        // console.log('pesos: ', pesos)
+        // const total = pesos.reduce((anterior, proximo) => {
+        //     return anterior + proximo;
+        // }, 0)
+
+        const minhaLista = [
+            ['Erick', 'Wendel'],
+            ['NodeBR', 'Nerdzão'],
+        ]
+        console.log('minhaLista ', minhaLista)
+        const total = minhaLista.meuReduce((anterior, proximo) => {
+            return anterior.concat(proximo)
+        }, [])
+        .join(', ')
+
+        console.log('total: ', total);
+        
+    } catch (error) {
+        console.log(`DEU RUIM: `, error)
+    }
+}
+
+main()
