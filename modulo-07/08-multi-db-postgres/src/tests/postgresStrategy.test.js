2245c26835e0b54cccfe89f1b67492b49c444828
Rafael Barros <rafaelbarros.df@gmail.com>
2019-02-16 19:50:43 -0200

    :construction: Listando Heróis - READ

--- modulo-07/08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ modulo-07/08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -10,21 +10,31 @@ const MOCK_HEROI_CADASTRAR = {
 
 
 
-describe('Postgres Strategy', function()  {
+describe('Postgres Strategy', function ()  {
     this.timeout(Infinity);
     
-    this.beforeAll(async function () {
+    this.beforeAll(async () => {
         await context.connect()
     })
 
-    it ('PostgresSQL Connection', async function () {
+    it ('PostgresSQL Connection', async () => {
         const result = await context.isConnected()
         assert.equal(result, true)
     })
 
-    it ('cadastrar', async function () {
+    it ('cadastrar', async () => {
         const result = await context.create(MOCK_HEROI_CADASTRAR)
         delete result.id
         assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
     })
+
+    it ('listar', async () => {
+        const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome})
+        // pegar a primeira posição
+        // const posicaoZero = result[0]
+        // const [posicao1, posicao2] = ['esse e o 1', 'esse eo 2']
+        delete result.id
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
+    })
+    
 })
