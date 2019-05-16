a788a131e553cb3b4dfc69b304abe198d6c02f3d
Rafael Barros <rafaelbarros.df@gmail.com>
2019-05-16 18:01:14 -0300

    :construction: Conhecendo o Mongoose

--- modulo-08/09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
+++ modulo-08/09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -1,12 +1,74 @@
 const ICrud = require('./interfaces/interfaceCrud')
+const Mongoose = require('mongoose')
+
+const STATUS = {
+    0: 'Disconectado',
+    1: 'Conectado',
+    2: 'Conectando',
+    3: 'Disconectando',
+}
 
 class MongoDb extends ICrud {
     constructor() {
         super()
+        this._herois = null
+        this._driver = null
     }
 
-    create(item) {
-        console.log('O item foi salvo em MongoDD')
+    async isConnected() {
+        const state = STATUS[ this._driver.readyState]
+        if (state === 'Conectado') return state
+
+        if (state !== 'Conectando') return state
+        
+        await new Promise(resolve => setTimeout(resolve, 1000))
+
+        return  STATUS[ this._driver.readyState]
+
+    }
+
+    defineModel() {
+
+        heroisSchema = new Mongoose.Schema({
+            nome: {
+                type: String,
+                required: true
+            },
+            poder: {
+                type: String,
+                required: true
+            },
+            insertAt: {
+                type: Date,
+                default: new Date()
+            }
+        })
+        
+        this._herois = Mongoose.model('herois', heroisSchema)
+
+    }
+
+    connect() {
+
+        Mongoose.connect('mongodb://rafaelbarros:root@192.168.99.100:27017/herois', 
+        { useNewUrlParser: true}, function (error) {
+            if (!error) return;
+    
+            console.log('Falha na conexão!', error)
+        })
+    
+        const connection = Mongoose.connection
+        connection.once('open', () => console.log('database rodando!!!'))
+
+        this._driver = connection
+    }
+
+    async create(item) {
+        const resultCadastrar = await model.create({
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        })
+        console.log('result cadastrar => ', resultCadastrar)
     }
 
 }
