3cd0c00b99ccac60c7b797f53e7cab64e5ce8b5a
Rafael Barros <rafaelbarros.df@gmail.com>
2019-05-21 23:39:27 -0300

    :construction: Configurando o JWT - plugins, testes e rota de login

---
+++ modulo-12/20-hapi-jwt/src/routes/authRoutes.js
@@ -0,0 +1,57 @@
+const BaseRoute = require('./base/baseRoutes')
+const Joi = require('joi')
+const Boom = require('boom')
+
+// npm i jsonwebtoken
+const Jwt = require('jsonwebtoken')
+
+const failAction = (request, headers, erro) => {
+    throw erro
+}
+const USER = {
+    username: 'manolodasilva',
+    password: '123'
+}
+
+class AuthRoutes extends BaseRoute {
+    constructor(secret) {
+        super()
+        this._secret = secret
+    }
+
+    login() {
+        return {
+            path: '/login',
+            method: 'POST',
+            config: {
+                auth: false,
+                tags: ['api'],
+                description: 'Obter token',
+                notes: 'faz login com user e senha do banco',
+                validate: {
+                    payload: {
+                        username: Joi.string().required(),
+                        password: Joi.string().required()
+                    }
+                }
+            },
+            handler: async (request) => {
+                const { username, password } = request.payload
+
+                if (username.toLowerCase() !== USER.username || password !== USER.password)
+                    return Boom.unauthorized()
+
+                const token = Jwt.sign({
+                    username: username,
+                    id: 1
+                }, this._secret)
+
+                return  {
+                    token
+                }
+            }
+        }
+    }
+}
+
+module.exports = AuthRoutes
